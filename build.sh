1eb625910e5bc06d17916b7e99ed158fc4c9dbf6
Diogo Simoes <diogo.miguel.simoes@gmail.com>
2021-10-21 16:13:09 +0100

    chore: circleci specific version

--- build.sh
+++ build.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
 
-# echo ${DOCKER_PASS} | docker login --username ${DOCKER_USER} --password-stdin
+echo ${DOCKER_PASS} | docker login --username ${DOCKER_USER} --password-stdin
 
-# HASH=$(git rev-parse --short HEAD)
-# VERSION=$(cat package.json | grep version | head -1 | awk -F ": " '{ print $2 }' | sed 's/[",]//g')
+HASH=$(git rev-parse --short HEAD)
+VERSION=$(cat package.json | grep version | head -1 | awk -F ": " '{ print $2 }' | sed 's/[",]//g')
 
-docker build -t cloudokihub/apisuite-storefront:latest .
-  # -t cloudokihub/apisuite-storefront:$VERSION .
-# docker push cloudokihub/apisuite-storefront:latest
-# docker push cloudokihub/apisuite-storefront:$VERSION
+docker build --build-arg SSH_PRIVATE_KEY="$(echo $GITHUB_SSH_PRIVATE_KEY_BASE64 | base64 -d)" \
+  -t cloudokihub/apisuite-storefront:latest \
+  -t cloudokihub/apisuite-storefront:$VERSION .
+docker push cloudokihub/apisuite-storefront:latest
+docker push cloudokihub/apisuite-storefront:$VERSION
